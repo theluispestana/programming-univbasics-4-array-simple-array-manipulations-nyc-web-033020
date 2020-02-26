4356f995b372082d231104697472bad74bf7f368
Luis Pestana <lpestana@protonmail.com>
2020-02-26 22:19:34 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+    array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(num)
+  num.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(arr, arr2)
+  arr.concat(arr2)
+end
+
+def using_insert(array, element)
+  array.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, int)
+  array.delete_at(int)
+end
